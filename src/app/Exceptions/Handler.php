14b08f054838fad76d87d5cb0a3b1bf3029dd46b
田畑颯 <chinarra.tabata@gmail.com>
2017-10-28 13:47:42 +0900

    Correct handler

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Log;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
