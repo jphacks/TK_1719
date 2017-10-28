bf318fddf37710d595c6c3a754ccd46c1fe1ae67
田畑颯 <chinarra.tabata@gmail.com>
2017-10-28 16:18:33 +0900

    Create models

---
+++ src/app/Repositories/ShelfRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Eloquent\User;
+use App\Repositories\Eloquent\Shelf;
+
+class ShelfRepository
+{
+    public function findOrFail(int $shelfId)
+    {
+        return Shelf::findOrFail($shelfId);
+    }
+
+    public function create(array $attributes)
+    {
+        return Shelf::create($attributes);
+    }
+
+    public function update(int $shelfId, array $attributes)
+    {
+        $shelf = $this->findOrFail($shelfId);
+        $shelf->fill($attributes);
+        $shelf->save();
+        return $shelf;
+    }
+
+    public function delete(int $shelfId)
+    {
+        return Shelf::destroy($shelfId);
+    }
+}
