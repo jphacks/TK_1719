436f64c96d7c40666b4bb89414b536d00cf9a791
田畑颯 <chinarra.tabata@gmail.com>
2017-10-28 19:22:10 +0900

    Create seeder

---
+++ src/database/seeds/CollectionsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Repositories\Eloquent\Collection;
+
+class CollectionsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        return Collection::create([
+            'url'      => 'localhost:8111',
+            'title'    => 'test',
+            'image'    => base_path("tests/Resources/upload.jpg"),
+            'shelf_id' => '1',
+        ]);
+    }
+}
