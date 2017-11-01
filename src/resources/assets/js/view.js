3cc95941d2bcd6373f158b8ef30b97e5c722ebc7
田畑颯 <chinarra.tabata@gmail.com>
2017-11-02 00:06:39 +0900

    Setup react router

--- src/resources/assets/js/view.js
+++ src/resources/assets/js/view.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Index from './components/Index'
+import { Route, BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-  <Index />,
-  document.getElementById('app')
-);
+ReactDOM.render((
+    <BrowserRouter>
+      <Route path="/" component={Index} />
+    </BrowserRouter>
+  ),
+  document.getElementById('app'));
