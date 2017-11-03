8a72df00369ca58b6bc82a1780e8c2f2dacac9ef
田畑颯 <chinarra.tabata@gmail.com>
2017-11-03 16:03:17 +0900

    Declare routing

--- src/resources/assets/js/view.js
+++ src/resources/assets/js/view.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Index from './components/Index'
 import { Route, BrowserRouter } from 'react-router-dom'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import Index from './components/Index'
+import Single from './components/Single'
+import Profile from './components/Profile'
 
 ReactDOM.render((
     <BrowserRouter>
-      <Route path="/" component={Index} />
+      <div>
+        <Header />
+        <Route exact path="/" component={Index} />
+        <Route path="/single" component={Single} />
+        <Route path="/profile" component={Profile} />
+        <Footer />
+      </div>
     </BrowserRouter>
   ),
   document.getElementById('app'));
