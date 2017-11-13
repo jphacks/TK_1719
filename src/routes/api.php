1199d9b68aa999940a3cfa0b1107bc2776a688da
田畑颯 <chinarra.tabata@gmail.com>
2017-11-13 09:00:07 +0900

    Config http method

--- src/routes/api.php
+++ src/routes/api.php
@@ -19,19 +19,19 @@ Route::middleware('api')->prefix('v1')->group(function () {
         Route::get('user/self', 'Api\V1\UserController@showSelf');
         Route::get('user/{user_id}/timeline', 'Api\V1\UserController@showTimeline');
         Route::get('user/{user_id}', 'Api\V1\UserController@showUser');
-        Route::post('user/{user_id}/update', 'Api\V1\UserController@update');
+        Route::put('user/{user_id}', 'Api\V1\UserController@update');
 
         Route::post('collection/store', 'Api\V1\CollectionController@store');
         Route::get('collection/{collection_id}', 'Api\V1\CollectionController@show');
-        Route::post('collection/{collection_id}/update', 'Api\V1\CollectionController@update');
-        Route::post('collection/{collection_id}/delete', 'Api\V1\CollectionController@delete');
+        Route::put('collection/{collection_id}', 'Api\V1\CollectionController@update');
+        Route::delete('collection/{collection_id}', 'Api\V1\CollectionController@delete');
         Route::post('collection/{collection_id}/shelf/{shelf_id}/attach', 'Api\V1\CollectionController@attach');
 
         Route::get('shelf/search', 'Api\V1\ShelfController@search');
-        Route::post('shelf/create', 'Api\V1\ShelfController@store');
+        Route::post('shelf', 'Api\V1\ShelfController@store');
         Route::get('shelf/{shelf_id}', 'Api\V1\ShelfController@show');
-        Route::post('shelf/{shelf_id}/update', 'Api\V1\ShelfController@update');
-        Route::post('shelf/{shelf_id}/delete', 'Api\V1\ShelfController@delete');
+        Route::put('shelf/{shelf_id}', 'Api\V1\ShelfController@update');
+        Route::delete('shelf/{shelf_id}', 'Api\V1\ShelfController@delete');
 
         Route::post('shelf/{shelf_id}/follow', 'Api\V1\ShelfController@follow');
         Route::post('shelf/{shelf_id}/refollow', 'Api\V1\ShelfController@refollow');
